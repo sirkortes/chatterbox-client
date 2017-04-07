70fb1bcf0036cc1ec5f0cf9d3ba5213af9d7c110
Hack Reactor Student <student@hackreactor.com>
2017-04-07 15:28:52 -0700

    Finished the send function for app.js

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,43 @@
 // YOUR CODE HERE:
+
+// $.get('http://parse.hrm8.hackreactor.com/chatterbox/classes/messages', function ( data ) {
+//  console.log("data",data);
+//  $('chats').html(data);
+// } )
+var url = 'http://parse.hrm8.hackreactor.com/chatterbox/classes/messages';
+var app = {};
+app.init = () =>  {
+
+};
+
+app.send = (data) => {
+  $.ajax({
+    type: 'POST',
+    url: url,
+    data: JSON.stringify(data),
+    success: () => { console.log('chatterbox: Message sent'); },
+    contentType: 'application/json',
+    error: () => { console.log('chatterbox: Message was not sent'); }
+  });
+};
+
+app.fetch = () => {
+
+};
+
+
+// Post to chatterbox
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
