07157baace11e75e7fd7f0d0ef18ce545379c75b
Hack Reactor Student <student@hackreactor.com>
2017-04-08 11:59:16 -0700

    Add rooms and limit messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-
-
+// http://parse.sfm8.hackreactor.com/chatterbox/classes/messages
   var App = function() {
     this.friends = [];
-    this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
-    // this.messages = [];
+    this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/' + this.room;
+    this.rooms = [];
+    this.messages = [];
+    this.renderIndex = 0;
+    this.room = "";
   };
 
   App.prototype.init = function() {
@@ -17,36 +19,42 @@ $(document).ready(function() {
     $('body').on('click', '.username', this.handleUsernameClick);
     $('.submit').on('click submit', this.handleSubmit);
 
-    console.log(this.messages);
-
-    var $compose_field = $('<div id="composing" class="placeholder" contenteditable="true" role="textbox"></div>');
-
-    $compose_field.appendTo($('#compose'));
-
-    $compose_field.on('keyup', function(e) {
-
-
+    $('#addNewRoom').on('keyup',function(e){
 
       var code = (e.keyCode ? e.keyCode : e.which);
-      if (code == 13) { //Enter keycode
-
-        // var composetext = $compose_field.text();
-        // $compose_field.text('');
-        // console.log("SUBMIT?",composetext);
-
-        $('.submit').trigger('click submit');
+      if (code == 13) { 
+        var room = $("#addNewRoom").val();
+        $("#addNewRoom").val('');
+        app.renderRoom(room);
       }
-    });
 
+    })
+      // create compose
+      var $compose_field = $('<div id="composing" class="placeholder" contenteditable="true" role="textbox"></div>');
+      $compose_field.appendTo($('#compose'));
+      $compose_field.on('keyup', function(e) {
+
+      // make compose send on return key
+      var code = (e.keyCode ? e.keyCode : e.which);
+      if (code == 13) { $('.submit').click(); }
+
+  });
+
+    // <script>
+    //   $('body').css('overflow','hidden !important');
+    //   $('<div style="background-color: white; color: white, min-height: 2000px !important; min-width: 2000px !important;></div>').appendTo('body');
+    // </script>
 
 
   };
 
   App.prototype.send = function(data) {
+    var app = this;
+    // console.log("this",this.server)
     $.ajax({
       type: 'POST',
-      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-      crossServer: true,
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/'+ app.room,
+      // crossServer: true,
       data: JSON.stringify(data),
       contentType: 'application/json',
       success: function() {
@@ -97,25 +105,82 @@ $(document).ready(function() {
     }
   };
 
-
-
   App.prototype.fetch = function() {
 
-    var context = this;
+    var app = this;
+
     var getEm = function() {
       $.ajax({
         type: 'GET',
-        url: context.server,
+        url: app.server,
         contentType: 'application/json',
         data: {
           order: "-createdAt"
         },
 
         success: function(data) {
+          // remember to store the rooms!
 
-          // console.log('messages fetched', data.results.length);
+          /*
+            username
+            text
+            roomName
+            objectId
+            createdAt
+          */
+
           if (data.results) {
-            context.getFeed(data.results);
+          // first time we get messages, we order them from oldest to newest ( message.createdat )
+            if ( app.messages.length === 0 ){
+
+                var messages = data.results.sort(function(a,b){ return b.createdAt - a.createdAt; });
+                // console.log("messages",messages);
+                app.messages = messages.slice(0,21);
+                app.getFeed(messages);
+
+            } else {
+
+              // on fetch, grab the message id of the last message we have stored
+              var lastMessage = app.messages[app.messages.length-1];
+              // console.log("lastMessage",lastMessage);
+
+              // find the index of that message in the data.messages 
+              var messages = data.results.sort(function(a,b){ return a.createdAt - b.createdAt  ; });
+              var matchLastMessageIndex;
+
+              messages.forEach(function(message,index){
+                if ( message.objectId === lastMessage.objectId ){ matchLastMessageIndex = index; }
+              });
+
+              // slice the messages from that index to newest
+              var newMessages = messages.reverse().slice(matchLastMessageIndex+1);
+              // console.log("newMessages",newMessages);
+
+              newMessages.forEach(function(message,index){
+
+                // push to our arr
+                app.messages.push(message);
+
+                // save room
+                if (!app.rooms.includes(message.roomname)){
+                  app.renderRoom(message.roomname);
+                }
+                // render it
+                app.renderMessage(message);
+              });
+              // push only that group to our messages
+
+              // cap our messages at 100
+              app.messages.slice(0,100);
+            }
+          // keep record of this.renderIndex to know where in our messages we must start rendering from
+          // on each new fetch, push to our messages array ( to end, so theyll have a greater index )
+          // then prepend to our #chat, starting from our renderIndex to current meesages.length index
+
+
+
+            // context.getFeed(data.results);
+
           } else {
             console.log("No data results", data);
           }
@@ -139,13 +204,11 @@ $(document).ready(function() {
 
     setInterval(function() {
 
-      context.clearMessages();
+      // app.clearMessages();
       getEm();
 
-    }, 5000);
-
+    }, 1000);
   };
-
 
   App.prototype.clearMessages = function() {
 
@@ -156,7 +219,7 @@ $(document).ready(function() {
     var htmlMessage = `
     <div class='message'>
       <a class="username" data-username="${message.username}">
-        ${message.username}
+        ${ this.sanitize(message.username) }
       </a>
       <p class="messageText">
         ${ this.sanitize(message.text) }
@@ -166,12 +229,21 @@ $(document).ready(function() {
       </p>
 
     </div>`;
-    $('#chats').append(htmlMessage);
+
+    $('#chats').prepend(htmlMessage);
+    while ( $('#chats').children().length > 20) {
+      //console.log($('#chats').children().length);
+      $('#chats').children().last().remove();
+    }
+    // increase render index
+    this.renderIndex++;
   };
 
   App.prototype.renderRoom = function(roomName) {
+
+    app.rooms.push(roomName);
     var htmlRoom = `<option value="${roomName}">${roomName}</option>`;
-    $('#roomSelect').append(htmlRoom);
+    $('#roomSelectInput').append(htmlRoom);
   };
 
 
@@ -187,9 +259,14 @@ $(document).ready(function() {
 
   App.prototype.handleSubmit = function() {
     console.log("SUBMITTING");
+
+    var composetext = $('#composing');
+    console.log("SUBMIT?",composetext);
+
     var message = {
-      username: window.user,
-      text: $('#composing').text()
+      username: window.location.search.slice(10),
+      text: composetext.text(),
+      roomname: $("#roomSelectInput").val()
     };
 
     // erase textbox
