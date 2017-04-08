e2a7d8b5dc9c6b3dd12f6c95f09ddd3775a99058
Hack Reactor Student <student@hackreactor.com>
2017-04-07 23:32:43 -0700

    Made it work!

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,27 +1,51 @@
 $(document).ready(function() {
 
 
-
-  // $.get('http://parse.hrm8.hackreactor.com/chatterbox/classes/messages', function ( data ) {
-  //  console.log("data",data);
-  //  $('chats').html(data);
-  // } )
-  // var url = 'http://parse.hrm8.hackreactor.com/';
-
   var App = function() {
     this.friends = [];
     this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+    // this.messages = [];
   };
 
   App.prototype.init = function() {
-    console.log("Initializing App...");
+
+    // console.log("Initializing App...");
+
     this.fetch();
+
+
+    $('body').on('click', '.username', this.handleUsernameClick);
+    $('.submit').on('click submit', this.handleSubmit);
+
+    console.log(this.messages);
+
+    var $compose_field = $('<div id="composing" class="placeholder" contenteditable="true" role="textbox"></div>');
+
+    $compose_field.appendTo($('#compose'));
+
+    $compose_field.on('keyup', function(e) {
+
+
+
+      var code = (e.keyCode ? e.keyCode : e.which);
+      if (code == 13) { //Enter keycode
+
+        // var composetext = $compose_field.text();
+        // $compose_field.text('');
+        // console.log("SUBMIT?",composetext);
+
+        $('.submit').trigger('click submit');
+      }
+    });
+
+
+
   };
 
   App.prototype.send = function(data) {
     $.ajax({
       type: 'POST',
-      url:  'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       crossServer: true,
       data: JSON.stringify(data),
       contentType: 'application/json',
@@ -34,32 +58,94 @@ $(document).ready(function() {
     });
   };
 
+  App.prototype.getFeed = function(messages) {
+    messages.forEach(function(message) {
+      app.renderMessage(message);
+    });
+  };
+
+  App.prototype.sanitize = function(s) {
+    if (s === ''){ return ' '};
+    if (s === undefined) {
+      return ' ';
+    };
+
+    return s.replace(/[^a-zA-Z0-9.-]/g, function(match) {
+      // return ' '+match[0].charCodeAt(0).toString(16)+' ';
+      return ' ';
+    });
+  };
+
+  App.prototype.timeAgo = function(timed) {
+
+    var time = new Date(timed);
+    milliseconds = time.getTime();
+    var now = new Date().getTime();
+    var elapsed = (now - milliseconds);
+    var secs = Math.ceil(elapsed / 1000);
+    var mins = Math.floor(secs / 60);
+    var hours = Math.floor(mins / 60);
+    var days = Math.floor(hours / 24);
+    if (days > 0) {
+      return days + "d";
+    } else if (hours > 0) {
+      return hours + "h";
+    } else if (mins > 0) {
+      return mins + "m";
+    } else {
+      return secs + "s";
+    }
+  };
+
+
+
   App.prototype.fetch = function() {
 
-    $.ajax({
-      type: 'GET',
-      url: this.server,
-      contentType: 'application/json',
+    var context = this;
+    var getEm = function() {
+      $.ajax({
+        type: 'GET',
+        url: context.server,
+        contentType: 'application/json',
+        data: {
+          order: "-createdAt"
+        },
 
-      success: function(data) {
-        console.log('chatterbox: Message fetched', data);
-        // data = JSON.parse(data);
-        data.results.forEach(function(message) {
-          console.log("message:", message);
-          App.prototype.renderMessage(message);
-        });
-      },
+        success: function(data) {
 
-      error: function(err) {
-        console.log('chatterbox: Message was not fetched', err);
-      }
+          // console.log('messages fetched', data.results.length);
+          if (data.results) {
+            context.getFeed(data.results);
+          } else {
+            console.log("No data results", data);
+          }
 
-    });
+          // console.log(context.messages.length,"got messages");
 
-    // $.get( App.prototype.server, function(data){
-    //   
-    // })
+
+          // render Messages
+          // check for all rooms, render rooms message.roomname
+
+        },
+
+        error: function(err) {
+          console.log('chatterbox: Message was not fetched', err);
+        }
+
+      });
+    };
+
+    getEm();
+
+    setInterval(function() {
+
+      context.clearMessages();
+      getEm();
+
+    }, 5000);
+
   };
+
 
   App.prototype.clearMessages = function() {
 
@@ -69,66 +155,61 @@ $(document).ready(function() {
   App.prototype.renderMessage = function(message) {
     var htmlMessage = `
     <div class='message'>
-      <a class="username" onclick="app.handleUsernameClick('${message.username}')">
+      <a class="username" data-username="${message.username}">
         ${message.username}
       </a>
       <p class="messageText">
-        ${message.text}
+        ${ this.sanitize(message.text) }
       </p>
       <p class="messageRoomName">
-        ${message.roomname}
+        ${message.roomname} @ ${ this.timeAgo( message.createdAt ) }
       </p>
+
     </div>`;
     $('#chats').append(htmlMessage);
   };
 
   App.prototype.renderRoom = function(roomName) {
-    var htmlRoom = `<div id='#${roomName}'></div>`;
+    var htmlRoom = `<option value="${roomName}">${roomName}</option>`;
     $('#roomSelect').append(htmlRoom);
   };
 
-  App.prototype.handleUsernameClick = function(userName) {
 
-    this.friends.push(userName);
+  App.prototype.handleUsernameClick = function(event) {
+
+    var username = $(this).data('username');
+    if (!app.friends.includes(username)) {
+      app.friends.push(username);
+    }
+    console.log("friends", app.friends);
+
   };
 
-  $('.submit').on('click', function(event){
-
-    App.prototype.handleSubmit();
-
-  });
-
   App.prototype.handleSubmit = function() {
+    console.log("SUBMITTING");
     var message = {
-      text: $('#message').val()
+      username: window.user,
+      text: $('#composing').text()
     };
+
+    // erase textbox
+    $('#composing').text('');
     console.log(message);
     App.prototype.send(message);
   };
 
-  // $('#send').on('click', function(event) {
 
-  // });
+  // Events
+  // <option value="saab">Saab</option>
+  /*
+      
+  */
 
 
-  // Post to chatterbox
-  // $.ajax({
-  //   // This is the url you should use to communicate with the parse API server.
-  //   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
-  //   type: 'POST',
-  //   data: JSON.stringify(message),
-  //   contentType: 'application/json',
-  //   success: function (data) {
-  //     console.log('chatterbox: Message sent');
-  //   },
-  //   error: function (data) {
-  //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-  //     console.error('chatterbox: Failed to send message', data);
-  //   }
-  // });
+
 
   app = new App();
   app.init();
-  console.log(app);
+  // console.log(app);
 
 });
